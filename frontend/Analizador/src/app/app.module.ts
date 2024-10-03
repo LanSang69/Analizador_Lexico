6c3a800049e39b56c429f903964bca0dbf827d3e
LanSang69 <alanisg029@gmail.com>
2024-10-02 19:57:24 -0600

    Interfaz basica

--- frontend/Analizador/src/app/app.module.ts
+++ frontend/Analizador/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { OptionsComponent } from './options/options.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    OptionsComponent
   ],
   imports: [
     BrowserModule,
